4043ef211752825d8e3c7e9faa24df84fb39dfea
Vaishali <v.sirimalla@somaiya.edu>
2024-05-02 21:58:01 +0530

    final_commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,14 +8,14 @@ const navLinks = [
     path: "/home",
     display: "Home",
   },
-  {
-    path: "/doctors",
-    display: "Find a Doctor",
-  },
-  {
-    path: "/services",
-    display: "Services",
-  },
+  // {
+  //   path: "/doctors",
+  //   display: "Find a Doctor",
+  // },
+  // {
+  //   path: "/services",
+  //   display: "Services",
+  // },
   {
     path: "/contact",
     display: "Contact",
